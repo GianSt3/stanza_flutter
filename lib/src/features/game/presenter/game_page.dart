f8bcdc9221982ee8c3f3380eff5a4c551f77c2b8
Gianluca <gianluca.st3@gmail.com>
2024-06-11 13:30:57 +0200

    feat: game page green screen

--- lib/src/features/game/presenter/game_page.dart
+++ lib/src/features/game/presenter/game_page.dart
@@ -11,53 +11,56 @@ class GamePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GameMessageListener(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            BlocBuilder<GameMessagesCubit, GameMessagesState>(
-              builder: (context, gameMessageState) {
-                return BlocBuilder<GameCubit, GameState>(
-                  builder: (context, gameState) {
-                    return Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Game ${gameState.status.maybeMap(start: (_) => "🔊", orElse: () => "🔇")}",
-                          style: Theme.of(context).textTheme.titleLarge,
-                        ),
-                        Text(
-                          gameMessageState.status.maybeMap(
-                              error: (err) => err.message, orElse: () => ""),
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodySmall
-                              ?.copyWith(color: Colors.red.shade900),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-            ),
-            const SizedBox(
-              height: 32,
-            ),
-            BlocBuilder<GameCubit, GameState>(
-              builder: (context, state) {
-                return Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: state.players
-                      .map((player) => GamePlayerWidget(
-                            player: player,
-                          ))
-                      .toList(),
-                );
-              },
-            ),
-          ],
+      child: Container(
+        color: const Color.fromARGB(255, 4, 244, 4),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              BlocBuilder<GameMessagesCubit, GameMessagesState>(
+                builder: (context, gameMessageState) {
+                  return BlocBuilder<GameCubit, GameState>(
+                    builder: (context, gameState) {
+                      return Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Game ${gameState.status.maybeMap(start: (_) => "🔊", orElse: () => "🔇")}",
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                          Text(
+                            gameMessageState.status.maybeMap(
+                                error: (err) => err.message, orElse: () => ""),
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall
+                                ?.copyWith(color: Colors.red.shade900),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              BlocBuilder<GameCubit, GameState>(
+                builder: (context, state) {
+                  return Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: state.players
+                        .map((player) => GamePlayerWidget(
+                              player: player,
+                            ))
+                        .toList(),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
